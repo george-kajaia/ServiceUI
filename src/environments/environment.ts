f5bdeff9b331d99e9aeff3a8752800fba0e1749d
giorgi-kajaia <giorgi-kajaia@yahoo.com>
2026-06-18 00:10:19 +0400

    Change Login Method

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // Base URL for Company/Investor/Product/Request/User controllers
-  apiBaseUrl: 'https://service-tokens.com/api'
-  //apiBaseUrl: 'https://localhost:7044/api'
+  //apiBaseUrl: 'https://service-tokens.com/api'
+  apiBaseUrl: 'https://localhost:7044/api'
 };
